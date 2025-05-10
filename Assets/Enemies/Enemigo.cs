a769ffae8eb4c3bfe445cebecd85f33f7f0a213e
Fausto Busso <faustobusso@gmail.com>
2025-05-10 16:03:18 -0300

    Actualizaciones
    Ataque del jugador a los enemigos

--- Assets/Enemies/Enemigo.cs
+++ Assets/Enemies/Enemigo.cs
@@ -2,20 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemigo : MonoBehaviour, IPresentacion
+public class Enemigo : MonoBehaviour, IPresentacion, ITakeDamage
 {
     public EnemyData enemyData;
+    private int vidaActual;
 
     private void Start()
     {
         if (enemyData != null)
         {
-            Debug.Log($"Enemigo Creado: {enemyData.nombre} con {enemyData.vida} de vida");
+            vidaActual = enemyData.vida;
+            Debug.Log($"Enemigo Creado: {enemyData.nombre} con {vidaActual} de vida");
         }
     }
 
     public void Accion()
     {
         Debug.Log($"{enemyData.nombre} dice: {enemyData.saludos}");
+    }
+
+    public void RecibirDaño(int cantidad)
+    {
+        vidaActual -= cantidad;
+        Debug.Log($"{enemyData.nombre} recibio {cantidad} de daño. Vida restante: {vidaActual}");
+
+        if (vidaActual <= 0)
+        {
+            Debug.Log($"{enemyData.nombre} ha sido derrotado");
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float velocidad = 5f;
+    public float rangoAtaque = 5f;
+    public int daño = 1;
 
     private void Update()
     {
@@ -16,6 +18,50 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R))
         {
             InteractuarConEnemigoMasCercano();
+        }
+
+        // Atacar enemigos
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            AtacarEnemigosCercanos();
+        }
+    }
+
+    void InteractuarConEnemigoMasCercano()
+    {
+        Enemigo[] enemigos = FindObjectsOfType<Enemigo>();
+
+        if (enemigos.Length == 0) return;
+
+        Enemigo enemigoMasCercano = null;
+        float distanciaMasCorta = Mathf.Infinity;
+
+        foreach (Enemigo enemigo in enemigos)
+        {
+            float distancia = Vector3.Distance(transform.position, enemigo.transform.position);
+            if (distancia < distanciaMasCorta)
+            {
+                distanciaMasCorta = distancia;
+                enemigoMasCercano = enemigo;
+            }
+        }
+
+        if (enemigoMasCercano != null)
+        {
+            enemigoMasCercano.Accion();
+        }
+    }
+
+    void AtacarEnemigosCercanos()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, rangoAtaque);
+        foreach (Collider col in colliders)
+        {
+            ITakeDamage objetivo = col.GetComponent<ITakeDamage>();
+            if (objetivo != null)
+            {
+                objetivo.RecibirDaño(daño);
+            }
         }
     }
 }
